b86e2e1caafa2370904dd1262c63035f1dd0c7f5
Kazi Anwar <kazisameen2702@gmail.com>
2023-11-29 00:32:26 -0500

    Pexels Page Text Based User Input

--- src/pexels.js
+++ src/pexels.js
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './styles.css';
 
 const Pexels = () => {
+  const [searchQuery, setSearchQuery] = useState('');
+
+  const handleSearch = () => {
+    // Implement your logic for fetching videos based on 'searchQuery'
+    console.log(`Searching for videos with query: ${searchQuery}`);
+  };
+
   return (
     <div>
       <h1>Pexels Page (Videos)</h1>
+      <input
+        type="text"
+        placeholder="Search for a video"
+        value={searchQuery}
+        onChange={(e) => setSearchQuery(e.target.value)}
+      />
+      <button onClick={handleSearch}>Search</button>
     </div>
   );
 };
