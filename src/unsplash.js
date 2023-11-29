5cb78f60a2bf7ae14d1b75787510df2554a4755a
Kazi Anwar <kazisameen2702@gmail.com>
2023-11-29 00:33:07 -0500

    Unsplash page text based user input

--- src/unsplash.js
+++ src/unsplash.js
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './styles.css';
 
 const Unsplash = () => {
+  const [searchQuery, setSearchQuery] = useState('');
+
+  const handleSearch = () => {
+    // Implement your logic for fetching images based on 'searchQuery'
+    console.log(`Searching for images with query: ${searchQuery}`);
+  };
+
   return (
     <div>
       <h1>Unsplash Page (Photos)</h1>
+      <input
+        type="text"
+        placeholder="Search for an image"
+        value={searchQuery}
+        onChange={(e) => setSearchQuery(e.target.value)}
+      />
+      <button onClick={handleSearch}>Search</button>
     </div>
   );
 };
