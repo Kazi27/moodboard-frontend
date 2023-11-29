a7c6d0316883d16709cbcdbf59beb9e6a1c40659
Kazi Anwar <kazisameen2702@gmail.com>
2023-11-28 22:47:12 -0500

    Made 4 components according & setup router

---
+++ src/pexels.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Pexels = () => {
+  return (
+    <div>
+      <h1>Pexels Page (Videos)</h1>
+    </div>
+  );
+};
+
+export default Pexels;
---
+++ src/unsplash.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Unsplash = () => {
+  return (
+    <div>
+      <h1>Unsplash Page (Photos)</h1>
+    </div>
+  );
+};
+
+export default Unsplash;
