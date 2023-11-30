d00d8424bf2e91ad307ecb5c91367991ceba3db2
Kazi Anwar <kazisameen2702@gmail.com>
2023-11-30 00:09:19 -0500

    kind of got api to work?

--- src/unsplash.js
+++ src/unsplash.js
@@ -1,12 +1,32 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './styles.css';
 
 const Unsplash = () => {
   const [searchQuery, setSearchQuery] = useState('');
+  const [images, setImages] = useState([]);
 
-  const handleSearch = () => {
-    //fetch images based on input from the api here
+  const handleSearch = async () => {
+    try {
+      const response = await fetch(
+        `https://api.unsplash.com/search/photos?page=1&query=${searchQuery}&client_id=Wp7H7sxUZlVfljqKikdpcey8Dg3b3OjE2TPv9qlGtwk`
+      );
+
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+
+      const data = await response.json();
+      setImages(data.results);
+    } catch (error) {
+      console.error('Error fetching images:', error.message);
+    }
   };
+
+  // useEffect to handle initial load (optional)
+  useEffect(() => {
+    // You can add code here to fetch initial data when the component mounts
+    // For example, fetch popular photos or any default query
+  }, []);
 
   return (
     <div>
@@ -18,6 +38,12 @@ const Unsplash = () => {
         onChange={(e) => setSearchQuery(e.target.value)}
       />
       <button onClick={handleSearch}>Search</button>
+
+      <div className="image-container">
+        {images.map((image) => (
+          <img key={image.id} src={image.urls.small} alt={image.description} />
+        ))}
+      </div>
     </div>
   );
 };
