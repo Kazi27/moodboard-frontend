f098037ebf490d6b0d3c89c9e232f91931cdfe68
Kazi Anwar <kazisameen2702@gmail.com>
2023-12-01 00:01:28 -0500

    fixing authorization

--- src/pexels.js
+++ src/pexels.js
@@ -1,11 +1,23 @@
 import React, { useState } from 'react';
 import './styles.css';
+import { createClient } from 'pexels'; // Import the createClient function from the pexels library
 
 const Pexels = () => {
   const [searchQuery, setSearchQuery] = useState('');
+  const [videos, setVideos] = useState([]);
 
   const handleSearch = () => {
-    //fetch videos based on input from the api here
+    // Replace 'YOUR_API_KEY' with your actual Pexels API key
+    const client = createClient('gikstuGIujwMYIlMaNSq5JrgjML80sNffCYwN8oqS2jWSCNOpmn6I7rj');
+
+    // Make a request to the video search endpoint
+    client.videos.search({ query: searchQuery, per_page: 10 }) // Adjust per_page as needed
+      .then(response => {
+        setVideos(response.videos);
+      })
+      .catch(error => {
+        console.error('Error fetching videos:', error.message);
+      });
   };
 
   return (
@@ -18,6 +30,18 @@ const Pexels = () => {
         onChange={(e) => setSearchQuery(e.target.value)}
       />
       <button onClick={handleSearch}>Search</button>
+
+      <div className="video-container">
+        {videos.map((video) => (
+          <div key={video.id} className="video-item">
+            <video controls width="100%">
+              <source src={video.video_files[0].link} type="video/mp4" />
+              Your browser does not support the video tag.
+            </video>
+            <p>{video.user.name}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
