6b0f05f0848ef58e666a83590d3a06e085a2a0a9
Rahatk1 <rahatkhandokar5@gmail.com>
2023-12-01 00:24:22 -0500

    fixed reducers

--- src/store/Store.js
+++ src/store/Store.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
-import parliamentReducer from '../slices/ParliamentSlice'; // change this
+import unsplashReducer from '../slices/UnsplashSlice'; // Make sure to import the unsplashReducer
 
+// A store is basically a container that holds the state of the entire application.
+// In this file, you're creating a redux store using reducers. Each reducer is assigned to a slice of the state.
 const Store = configureStore({ 
-    reducer: {
-      parliament: parliamentReducer,
-      congress: congressReducer
-    },
-  });
-  export default Store;
+  reducer: {
+    unsplash: unsplashReducer // Add the unsplash reducer here
+  },
+});
+
+export default Store;
