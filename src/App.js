ec4f654681afb938c5e2d37cfd48e207616063e3
DavidA123777 <ddda4167@gmail.com>
2023-12-01 00:37:54 -0500

    changed some filenames which were producing errors

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 import { configureStore } from '@reduxjs/toolkit';
 import { Provider } from 'react-redux';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import unsplashReducer from './unsplashSlice'; // Import the unsplash reducer
+import unsplashReducer from './store/slices/unsplashSlice'; // Import the unsplash reducer
 import Homepage from './homepage';
 import Unsplash from './unsplash';
 import Pexels from './pexels';
