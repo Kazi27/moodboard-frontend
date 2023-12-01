9fe17dc98655f76c6267ffdc95b8ed4dd7f8e32f
Rahatk1 <rahatkhandokar5@gmail.com>
2023-12-01 00:19:44 -0500

    unsplash slice

--- src/store/slicies/unSplashSlice.js
+++ src/store/slicies/unSplashSlice.js
@@ -1,23 +1,19 @@
+// unsplashSlice.js
 import { createSlice } from '@reduxjs/toolkit';
 
-//this file is pretty similar to congressslice.js except the selector, u use createSlice to define a portion of the Redux state named 'parliament' w/ initial state & reducers
-//next up u have ur reducers (setMemberInfo, setInterests, setError) that define how the state is updated to different actions
-//they modify the state based on the provided payload and then you export this, could've used a selector here too tbh
-const parliamentSlice = createSlice({
-  name: 'parliament',
+export const unsplashSlice = createSlice({
+  name: 'unsplash',
   initialState: {
-    memberInfo: null,
-    interests: [],
+    searchQuery: '',
+    images: [],
     error: null,
   },
   reducers: {
-    setMemberInfo(state, action) {
-      state.memberInfo = action.payload;
-      state.error = null;
+    setSearchQuery(state, action) {
+      state.searchQuery = action.payload;
     },
-    setInterests(state, action) {
-      state.interests = action.payload;
-      state.error = null;
+    setImages(state, action) {
+      state.images = action.payload;
     },
     setError(state, action) {
       state.error = action.payload;
@@ -25,5 +21,6 @@ const parliamentSlice = createSlice({
   },
 });
 
-export const { setMemberInfo, setInterests, setError } = parliamentSlice.actions;
-export default parliamentSlice.reducer;
+export const { setSearchQuery, setImages, setError } = unsplashSlice.actions;
+
+export default unsplashSlice.reducer;
