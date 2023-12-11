f8b9489a5b748bdde469c859dffbb7e85d099642
Rahatk1 <rahatkhandokar5@gmail.com>
2023-12-11 01:17:22 -0500

    DYNAMIC TEAM SEGMENT

--- src/App.js
+++ src/App.js
@@ -3,8 +3,9 @@ import { Provider } from 'react-redux';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { configureStore } from '@reduxjs/toolkit';
 
-import unsplashReducer from './store/slices/unsplashSlice'; 
+import unsplashReducer from './store/slices/unsplashSlice';
 import pexelsReducer from './store/slices/pexelsSlice';
+import Team from './team'; // Import the Team component
 
 import Homepage from './homepage';
 import Unsplash from './unsplash';
@@ -13,6 +14,7 @@ import Favorites from './favorites';
 import Navbar from './navbar';
 import EditFavorities from './editFavorites';
 
+// Configure Redux store
 const store = configureStore({
   reducer: {
     unsplash: unsplashReducer,
@@ -30,7 +32,9 @@ function App() {
           <Route path="/photos" element={<Unsplash />} />
           <Route path="/videos" element={<Pexels />} />
           <Route path="/favorites" element={<Favorites />} />
-          <Route path="/edit-favorites" element={<EditFavorities/>}/>
+          <Route path="/edit-favorites" element={<EditFavorities />} />
+          <Route path="/team" element={<Team />} /> {/* This line added for /team route */}
+          {/* Add other specific routes as needed */}
         </Routes>
       </Router>
     </Provider>
