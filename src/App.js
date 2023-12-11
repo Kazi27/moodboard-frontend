b4c5c9402a04154575c34c10e0bad59f01257610
Rahatk1 <rahatkhandokar5@gmail.com>
2023-12-11 00:47:48 -0500

    made some dynamic segmentation

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
-// App.js
 import React from 'react';
-import { configureStore } from '@reduxjs/toolkit';
 import { Provider } from 'react-redux';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-// import the unsplash and pexels reducers
+import { configureStore } from '@reduxjs/toolkit';
+
 import unsplashReducer from './store/slices/unsplashSlice'; 
 import pexelsReducer from './store/slices/pexelsSlice';
 
@@ -12,11 +11,8 @@ import Unsplash from './unsplash';
 import Pexels from './pexels';
 import Favorites from './favorites';
 import Navbar from './navbar';
-import Team from './team'
 import EditFavorities from './editFavorites';
 
-
-// configure Redux store for pexels and unsplash reducers
 const store = configureStore({
   reducer: {
     unsplash: unsplashReducer,
@@ -34,10 +30,8 @@ function App() {
           <Route path="/photos" element={<Unsplash />} />
           <Route path="/videos" element={<Pexels />} />
           <Route path="/favorites" element={<Favorites />} />
-          <Route path="/:team" element={<Team />} />
           <Route path="/edit-favorites" element={<EditFavorities/>}/>
         </Routes>
-        <Team />
       </Router>
     </Provider>
   );
--- src/homepage.js
+++ src/homepage.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './homepage.css'; // Make sure the path is correct
+import Team from './team'; // Import the Team component
 
 const Homepage = () => {
   return (
     <div className="homepage-container">
-      <h1> </h1>
+      <h1>Welcome to Our Moodboard</h1>
+      <Team /> {/* Include the Team component here */}
     </div>
   );
 };
